fb7b8d31adb1a1a531a234fd0f527dd5277fd573
Stefano DC <mikesh07mail@gmail.com>
2017-04-20 19:10:36 +0200

    reorganize src file structure

---
+++ src/Functions/Review.java
@@ -0,0 +1,17 @@
+public class Review {
+
+    // Costruttore
+    public Review () {
+
+    }
+
+    // Prende tutte le recensioni dal DB
+    public void getReview() {
+
+    }
+
+    // Funzione per l'invio delle review
+    public void setReview(String re) {
+
+    }
+}
---
+++ src/Rating.java
@@ -0,0 +1,17 @@
+public class Rating {
+
+    // Costruttore
+    public Rating () {
+
+    }
+
+    // Prende tutte le recensioni dal DB
+    public void getRating() {
+
+    }
+
+    // Funzione per l'invio delle review
+    public void setRating(String ra) {
+
+    }
+}
