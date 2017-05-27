cfd4e4ee6d56529091f7394faa964b4ac28f5898
FilippoSanti <fil.san93.fs@gmail.com>
2017-05-27 12:12:06 +0200

    fixed view

--- untitledGaming/src/presentation/Loggato.java
+++ untitledGaming/src/presentation/Loggato.java
@@ -6,10 +6,11 @@ import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.Window.Type;
 
 public class Loggato {
 
-	private JFrame frame;
+	private JFrame frmUntitledGaming;
 
 	/**
 	 * Launch the application.
@@ -19,7 +20,7 @@ public class Loggato {
 			public void run() {
 				try {
 					Loggato window = new Loggato();
-					window.frame.setVisible(true);
+					window.frmUntitledGaming.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -38,41 +39,51 @@ public class Loggato {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 750, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmUntitledGaming = new JFrame();
+		frmUntitledGaming.setResizable(false);
+		frmUntitledGaming.setTitle("   Untitled Gaming  -  (Username Here!)");
+		frmUntitledGaming.setBounds(100, 100, 750, 500);
+		frmUntitledGaming.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmUntitledGaming.getContentPane().setLayout(null);
 		
 		JLabel label = new JLabel("");
 		label.setBounds(0, 0, 46, 14);
-		frame.getContentPane().add(label);
+		frmUntitledGaming.getContentPane().add(label);
 		
 		JLabel lblNewLabel = new JLabel("Hello,");
 		lblNewLabel.setForeground(Color.GRAY);
 		lblNewLabel.setFont(new Font("Oregano", Font.PLAIN, 15));
-		lblNewLabel.setBounds(573, 22, 38, 25);
-		frame.getContentPane().add(lblNewLabel);
+		lblNewLabel.setBounds(559, 22, 52, 25);
+		frmUntitledGaming.getContentPane().add(lblNewLabel);
 		
-		JLabel label_1 = new JLabel("__________");
-		label_1.setForeground(Color.DARK_GRAY);
-		label_1.setFont(new Font("Oregano", Font.PLAIN, 15));
-		label_1.setBounds(609, 25, 115, 19);
-		frame.getContentPane().add(label_1);
+		JLabel lblusernameHere = new JLabel("-Username Here!-");
+		lblusernameHere.setForeground(Color.DARK_GRAY);
+		lblusernameHere.setFont(new Font("Oregano", Font.PLAIN, 15));
+		lblusernameHere.setBounds(598, 25, 126, 19);
+		frmUntitledGaming.getContentPane().add(lblusernameHere);
 		
 		JLabel lblNewLabel_1 = new JLabel("Untitled Gaming");
 		lblNewLabel_1.setFont(new Font("Vivaldi", Font.BOLD, 30));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setBounds(0, 68, 734, 37);
-		frame.getContentPane().add(lblNewLabel_1);
+		frmUntitledGaming.getContentPane().add(lblNewLabel_1);
 		
 		JButton btnNewButton = new JButton("Il Tuo Profilo");
+		btnNewButton.setToolTipText("Il Tuo Profilo");
 		btnNewButton.setFont(new Font("MV Boli", Font.ITALIC, 17));
 		btnNewButton.setBounds(82, 232, 174, 64);
-		frame.getContentPane().add(btnNewButton);
+		frmUntitledGaming.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Gioca Ora");
+		btnNewButton_1.setToolTipText("Gioca Ora");
 		btnNewButton_1.setFont(new Font("MV Boli", Font.ITALIC, 17));
 		btnNewButton_1.setBounds(471, 232, 174, 64);
-		frame.getContentPane().add(btnNewButton_1);
+		frmUntitledGaming.getContentPane().add(btnNewButton_1);
+		
+		JButton btnLogOut = new JButton("Log Out");
+		btnLogOut.setToolTipText("Log Out");
+		btnLogOut.setFont(new Font("MV Boli", Font.PLAIN, 12));
+		btnLogOut.setBounds(587, 58, 85, 23);
+		frmUntitledGaming.getContentPane().add(btnLogOut);
 	}
 }
