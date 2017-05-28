e2e8b17605d3eb635adda25a16caa109b79a92ba
Davideub <davide.ubaldi@outlook.it>
2017-05-28 11:01:14 +0200

    Test delle nuove funzioni

--- untitledGaming/src/presentation/testClass.java
+++ untitledGaming/src/presentation/testClass.java
@@ -8,17 +8,29 @@ public class testClass {
     public static void main(String[] args) throws SQLException {
 
         // Registration
-        System.out.println("Registration = " + controller.eventsListener.insertUser("mikesh", "password", "stefano",
-                "de ciantis", "m@a.il","user"));
+        System.out.println("Registration = " + controller.eventsListener.insertUser("davideu", "abcd", "davide",
+                "ubaldi", "ca@a.il","user"));
 
         // Login
-        System.out.println("Login = " + controller.eventsListener.userAuth("test_userx","password"));
+        System.out.println("Login = " + controller.eventsListener.userAuth("davideu","abcd"));
 
         // User info
-        System.out.println(controller.eventsListener.getUserInfo("mikesh"));
+        System.out.println(controller.eventsListener.getUserInfo("davideu"));
 
-        // User stats
-        System.out.println(controller.eventsListener.getUserStats("mikesh"));
+
+
+        // Add review
+        System.out.println ("Review aggiunta = "+ controller.eventsListener.addReview("davideu", "Bene ma non benissimo", 3.5 ));
+
+        // See pending reviews
+        System.out.println(controller.eventsListener.getPendingReview());
+
+        // Approve review
+        System.out.println(controller.eventsListener.approveReview("davideu"));
+
+        //check approved reviews
+        System.out.println(controller.eventsListener.getApprovedReview());
+
     }
 
 }
