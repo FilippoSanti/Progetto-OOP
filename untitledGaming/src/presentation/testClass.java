92c5a157d0490d09ba51997ae4cdc9e42f3d5005
Stefano De Ciantis <mikesh07mail@gmail.com>
2017-06-07 15:51:52 +0200

    add getImg() & storeImg() methods

--- untitledGaming/src/presentation/testClass.java
+++ untitledGaming/src/presentation/testClass.java
@@ -1,22 +1,77 @@
 package presentation;
 
 import business.implementation.DBManager;
-import controller.eventsListener;
 
-import java.sql.Date;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+import java.io.*;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class testClass {
 
-    public static void main(String[] args) throws SQLException {
-
+    public static boolean storeImg () {
         try {
-            Date data = DBManager.stringToDate("11-11-1990");
 
-        System.out.println(eventsListener.addTimeline(20, 1, data , 100));
-    } catch (ParseException e) {
-        e.printStackTrace();
+            // Open the connection
+            Connection con       = business.implementation.DBManager.Connect();
+            Statement  st        = con.createStatement();
+            File       imgfile   = new File("pic.jpg");
+
+            FileInputStream fin  = new FileInputStream(imgfile);
+
+            // Prepare the query
+            PreparedStatement pre =
+                    con.prepareStatement("insert into Image values(?,?,?)");
+
+            pre.setString(1,"test");
+            pre.setInt   (2,3);
+
+            // Store the image into the db
+            pre.setBinaryStream(3,(InputStream)fin,(int)imgfile.length());
+            pre.executeUpdate();
+            System.out.println("Successfully inserted the file into the database!");
+
+            // Close the connection
+            pre.close();
+            con.close();
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-} }
+
+        return false;
+    }
+
+    public static boolean getImg() {
+        try {
+
+            // Open the connection
+            Connection con = DBManager.Connect();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select image from image");
+
+            int i = 0;
+            while (rs.next()) {
+                InputStream in = rs.getBinaryStream(1);
+                OutputStream f = new FileOutputStream(new File("test"+i+".jpg"));
+                i++;
+                int c = 0;
+                while ((c = in.read()) > -1) {
+                    f.write(c);
+                }
+                f.close();
+                in.close();
+
+                return true;
+            }
+        } catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return false;
+    }
+
+}
